2c6d42b4aa2cb2ab98da219d98e164e29f391b8c
Tomas Romero <canotto90@gmail.com>
2015-04-23 20:27:30 -0300

    Show user's name as picture's title

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'canotto90:live-users',
-  version: '0.0.2',
+  version: '0.0.4',
   summary: 'Show a live snapshot of all users with an active session',
   git: 'https://github.com/taromero/live-users',
   documentation: 'README.md'
