79c2728120232128e56f4240144d4cacf749f9fb
Tomas Romero <canotto90@gmail.com>
2015-05-04 00:03:57 -0300

    Update session variable with current snapshot

--- client/lib/camera.js
+++ client/lib/camera.js
@@ -6,7 +6,9 @@ Camera = {
   takeSnapshot: function() {
     if (Camera.video.src) {
       Camera.canvasCtx.drawImage(Camera.video, 0, 0, 116, 92) // (src, x, y, width, height)
-      return Camera.canvas.toDataURL('image/jpeg')
+      var imageDataUrl = Camera.canvas.toDataURL('image/jpeg')
+      Session.set('live_users.current_user_snapshot', imageDataUrl)
+      return imageDataUrl
     }
   },
   keepUserSnapshotUpdated: function() {
